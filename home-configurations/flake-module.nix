ab4ac4a138e9f452d26b4bd83c5c5a97435ab808
Sean Gillespie <sean@mistersg.net>
2026-06-22 15:04:18 -0400

    nix fmt

--- home-configurations/flake-module.nix
+++ home-configurations/flake-module.nix
@@ -3,14 +3,13 @@
   withSystem,
   ...
 }: {
-  flake = 
-    let
-      system = "x86_64-linux";
-    in {
-      homeConfigurations.sgillespie = withSystem system ({pkgs, ...}:
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./default.nix ];
-        });
-    };
+  flake = let
+    system = "x86_64-linux";
+  in {
+    homeConfigurations.sgillespie = withSystem system ({pkgs, ...}:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [./default.nix];
+      });
+  };
 }
