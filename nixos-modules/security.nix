e592a99c93e0da9efb880433a034ad1194e4c150
Sean D Gillespie <sean@mistersg.net>
2026-07-20 08:19:17 -0400

    Add sudo and set wheel to NOPASSWD

--- nixos-modules/security.nix
+++ nixos-modules/security.nix
@@ -3,16 +3,23 @@
   lib,
   ...
 }: {
-  security.pam = lib.mkIf config.security.pam.u2f.enable {
-    u2f.settings = {
-      authfile = "/etc/u2f_keys";
-      control = "sufficient";
+  security = {
+    pam = lib.mkIf config.security.pam.u2f.enable {
+      u2f.settings = {
+        authfile = "/etc/u2f_keys";
+        control = "sufficient";
+      };
+
+      services = {
+        login.u2f.control = "required";
+        greetd.u2f.control = "required";
+        swaylock.u2f.control = "required";
+      };
     };
 
-    services = {
-      login.u2f.control = "required";
-      greetd.u2f.control = "required";
-      swaylock.u2f.control = "required";
+    sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
     };
   };
 }
